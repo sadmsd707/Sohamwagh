305b37d721bfba27acf3e2937250134eb728d6f9
Shiv <sadmsd707@gmail.com>
2026-07-27 11:46:54 +0530

    Update compare.c

--- compare.c
+++ compare.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
-
 void compare(char[], char[]);
-
 int i;
-
 int main() {
     char str1[50], str2[50];
-
     printf("Enter First String: ");
     scanf("%s", str1);
-
     printf("Enter Second String: ");
     scanf("%s", str2);
-
     compare(str1, str2);
-
     return 0;
 }
-
 void compare(char str1[50], char str2[50]) {
     i = 0;
-
     while (str1[i] != '\0' && str2[i] != '\0') {
         if (str1[i] != str2[i]) {
             break;
         }
         i++;
     }
-
     if (str1[i] == str2[i]) {
         printf("Both Strings are Equal\n");
     }
@@ -37,6 +27,4 @@ void compare(char str1[50], char str2[50]) {
     else {
         printf("First String is Smaller than Second String\n");
     }
-
-   
 }
