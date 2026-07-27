78d521b7fa7730e1fdb3e05f78d1b1a6f9e2e083
Shiv <sadmsd707@gmail.com>
2026-07-27 11:46:04 +0530

    Fix formatting and remove unnecessary whitespace

--- length.c
+++ length.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
-
 int length(char[]);
-
 int main() {
     char str[50];
-
     printf("Enter String: ");
     scanf("%s", str);
-
     printf("Length of the String is %d", length(str));
-
     return 0;
 }
-
 int length(char a[]) {
     int len = 0;
