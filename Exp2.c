8dc54c432af57b88f71d9a6aa720eb7708e98140
Shiv <sadmsd707@gmail.com>
2026-08-03 11:20:24 +0530

    Create Exp2.c

---
+++ Exp2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int arr[5];
+    printf("Enter 5 integers:\n");
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Entered arr: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
