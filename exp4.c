a05af1cd14dd2e2d8fd5ecbea13ab9fddf0cbb68
Shiv <sadmsd707@gmail.com>
2026-08-31 11:51:44 +0530

    Update exp4.c

--- exp4.c
+++ exp4.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
-struct student{
+struct student {
     char name[10];
     int roll_no;
-    int marks;
+    int marks; 
 };
 
 int main() {
-    struct student s[3];
+    struct student s[5];
+    printf("Enter Information of 5 Students:\n");
     
-    for(int i=0;i<3;i++) {
-         printf("Student Name:");
-         scanf("%s",&s[i].name);
-         printf("Enter Roll No:");
-         scanf("%d",&s[i].roll_no);
-         printf("Enter Marks:");
-         scanf("%f",&s[i].marks);
+    for(int i = 0; i < 5; i++) {
+        printf("\nStudent %d", i + 1);
+        printf("\nStudent Name: ");
+        scanf("%s", s[i].name);
+        
+        printf("Enter Roll No: ");
+        scanf("%d", &s[i].roll_no);
+        
+        printf("Enter Marks: ");
+        scanf("%d", &s[i].marks); 
     }
-
+    
+    printf("\n--- Student Details ---\n");
+    for(int i = 0; i < 5; i++) {
+        printf("\nStudent %d", i + 1);
+        printf("\nName: %s", s[i].name);
+        printf("\nRoll No: %d", s[i].roll_no);
+        printf("\nMarks: %d\n", s[i].marks);
+    }
+    
     return 0;
-
 }
+
+
